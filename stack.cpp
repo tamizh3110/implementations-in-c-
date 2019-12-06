e9c6c1a1cce5c5792afb8cd13dbbe554140921fb
tamizhselvan venkatesan <tamizh3110@gmail.com>
2019-12-06 15:57:12 -0500

    implementation of stack

---
+++ stack.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#define MAX_SIZE 5
+using namespace std;
+
+struct stack{
+	int items[MAX_SIZE];
+	int top;
+	stack(){
+		top=0;
+	}
+	void push(int data);
+	int pop();
+	int peek();
+};
+
+
+void stack::push(int data){
+	if(top>MAX_SIZE){
+		printf("%s","overflow");
+	}else{
+		items[top++]=data;
+	}
+}
+
+int stack::pop(){
+	if(top<=-1){
+		printf("%s","underflow");
+		return 0;
+	}else{
+		int val=items[top--];
+		return val;
+	}
+}
+
+int stack::peek(){
+	return items[top];
+}
+
+int main(int argc,char* argv[]){
+	stack s;
+	s.push(10);
+	s.push(20);	
+	s.push(30);
+	s.push(40);
+	s.push(50);
+	//s.pop();
+	for(int i=0;i<s.top;i++){
+		cout<<s.items[i]<<endl;
+	}
+	return(0);
+}
+
