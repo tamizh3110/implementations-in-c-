3428c0c7b93fd0e48aeae3da9af491a0db9d4b10
tamizhselvan venkatesan <tamizh3110@gmail.com>
2019-12-27 00:14:19 -0500

    in progress

---
+++ merge_sort.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+
+using namespace std;
+
+void merge_sort(){
+
+	
+
+}
+
+
+void merge(){
+
+
+}
+
+int main(int argc,char *argv[]){
+
+	int *input_array;
+	int size=0;
+
+	cout<<"enter input array input size"<<endl;
+	cin>>size>>endl;
+
+	cout<<"Allocating memory"<<endl;
+	input_array=(int*)malloc(size*sizeof(int));
+
+	cout<<"enter array input contents"<<endl;
+	
+	for(int i=0;i<size;i++){
+	
+		cin>>input_array[i];
+	}
+	
+	int start_index=0,end_index=size-1;
+
+	while(start<=end){
+		
+		
+	}
+
+
+
+
+
+
+
+	
+	return(0);
+}
